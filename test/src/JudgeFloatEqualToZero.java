130f0383276a2ce671d553eff1095cd5049d200d
GraceeFlower <1491644103@qq.com>
2020-02-08 14:51:55 +0800

    [Gracee] fix: reload the tests

--- test/src/JudgeFloatEqualToZero.java
+++
@@ -1,9 +0,0 @@
-public class JudgeFloatEqualToZero {
-  public static void main(String[] args) {
-    float num = 1e-6F;
-
-    System.out.println(Math.abs(num) < 1e-6F ?
-        "This floating-point number is equal to zero" :
-        "This floating-point number is not equal to zero");
-  }
-}
